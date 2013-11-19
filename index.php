07d89d1e2bc7cc4c2f40cba19220b5ac839d4f29
amcgowan <andr3w.mcg0wan@gmail.com>
2013-11-18 17:59:14 -0800

    Setup coming soon page

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Andrew McGowan | Web Developer | Portfolio
+    </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="I am Andrew McGowan, a web developer in the Fraser Valley, BC, I specialize in front end development, however I also am a solid programmer for backend work and game development. Canadian lower mainland abbotsford web developer.">
+    <meta name="keywords" content="Andrew McGowan, Andrew, McGowan, Web Developer, Fraser Valley, Abbotsford, Lower Mainland, Canada, Canadian, BC, British Columbia, Programmer, HTML5, CSS3, JavaScript, PHP, MySQL, Designer, Design">
+    <meta name="author" content="Andrew McGowan">
     <!-- Bootstrap -->
     <link href="styles/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link href="styles/mcgowan.css" rel="stylesheet" media="screen">
@@ -15,317 +19,34 @@
     <![endif]-->
   </head>
   <body>
-	  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-  	  	<div class="container">
-	  		<div class="navbar-header">
-	  			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".am-navbar">
-	  				<span class="sr-only">Toggle navigation</span>
-	  				<span class="icon-bar"></span>
-	  				<span class="icon-bar"></span>
-	  				<span class="icon-bar"></span>
-	  			</button>
-	  			<a class="navbar-brand" href="#">GoonWorx</a>
+	 
+  	<div class="container">
+  		<div class="masthead">
+  			<h3 class="text-muted">Andrew C. McGowan</h3>
+  		</div>
+  		<div class="jumbotron">
+  			
+	  		<h1>Hello!</h1>
+	  		<p class="lead">
+		  		Welcome to my portfolio. There is nothing here yet, but there will be lots to display real soon. If you are interested in seeing some samples of my work right away feel free to get in touch with me. 
+	  		</p>
+	  		<a class="btn btn-lg btn-success" href="mailto:andrew@goonworx.com">Contact Me</a>
+	  	</div>
+	  	<div class="row">
+	  		<div class="col-md-4">
+	  			<a target="_blank" href="http://www.facebook.com/andrew.mcgowan.90"><i class="fa fa-facebook-square"></i></a>
 	  		</div>
-	  		<div class="collapse navbar-collapse am-navbar pull-right">
-	  			<ul class="nav navbar-nav">
-	  				<li>
-	  					<a href="#">Portfolio</a>
-	  				</li>
-	  				<li>
-	  					<a href="#">Work Eperiance</a>
-	  				</li>
-	  				<li>
-	  					<a href="#">About</a>
-	  				</li>
-	  				<li>
-	  					<a href="#">Contact</a>
-	  				</li>
-	  			</ul>
+	  		<div class="col-md-4">
+	  			<a target="_blank" href="http://www.linkedin.com/pub/andrew-mcgowan/14/7b/ab1"><i class="fa fa-linkedin-square"></i></a>
 	  		</div>
-  	  	</div>
-  	</nav>
-  	<div class="jumbotron">
-  		<div class="container">
-	  		<h1>Andrew Craig McGowan</h1>
-	  		<p class="lead">Web developer in the Fraser Valley specializing in front end design and development</p>
+	  		<div class="col-md-4">
+	  			<a target="_blank" href="http://vimeo.com/channels/615719"><i class="fa fa-vimeo-square"></i></a>
+	  		</div>
+	  	</div>
+	  	<div class="footer">
+	  		<p>&copy; <a href="http://www.goonworx.com/" target="_blank">GoonWorx</a> <?= date('Y'); ?></p>
 	  	</div>
   	</div>
-  	<div class="container technologies">
-  		<article class="row">
-	  		<div class="col-md-3 col-sm-6">
-	  			<div class="displays">
-	  				<div  class="center">
-	  					<i class="fa fa-desktop desktop"></i>
-	  					<i class="fa fa-laptop laptop"></i>
-	  					<i class="fa fa-mobile-phone mobile-phone"></i>
-	  				</div>
-	  			</div>
-	  			<h2>Less/CSS</h2>
-	  			<p>Uses Less to create clean and efficient style sheets that let your site display beautifully on any size device</p>
-	  		</div>
-	  		<div class="col-md-3 col-sm-6">
-	  			<i class="fa fa-code"></i>
-	  			<h2>HTML</h2>
-	  			<p>Your site will be developed with HTML5 to allow for a tidy site structure and rich user experience.</p>
-	  		</div>
-	  		
-	  		<div class="clearfix visible-sm"></div>
-	  		
-	  		<div class="col-md-3 col-sm-6">
-	  			<i class="fa fa-rocket"></i>
-	  			<h2>JavaScript</h2>
-	  			<p>JavaScript in conjunction with jQuery will provide wonderful functionality to your site.</p>
-	  		</div>
-	  		<div class="col-md-3 col-sm-6">
-	  			<i class="fa fa-cogs"></i>
-	  			<h2>PHP/SQL</h2>
-	  			<p>Not only will your site look good, but it can connect to a database do blah blh blha</p>
-	  		</div>
-  		</article>
-  	</div>
-    <section id="recent-work" class="slider">  
-	   <div class="container">
-	   		<h2>Recent Work
-		   		
-	   		</h2>
-	   		<article class="row slide-right rubrix">
-	   			<div class="hidden-xs col-sm-6 col-md-3">
-	   				<div class="thumbnail">
-	   					<img src="images/rubrix-1.jpg" width="400" height="587">
-	   				</div>
-	   				<div class="thumbnail">
-	   					<img src="images/rubrix-2.jpg" width="400" height="587">
-	   				</div>
-	   				<div class="thumbnail">
-	   					<img src="images/rubrix-3.jpg" width="400" height="587">
-	   				</div>
-	   			</div>
-	   			<div class="col-xs-12 col-sm-6 col-md-6">
-	   				<h3>Rubrix
-	   					<a class="slide-back pull-right hint" title="Back">
-		   					<i class="fa fa-chevron-circle-left"></i>
-	   					</a>
-	   				</h3>
-	   				<div class="col-xs-6 pull-left visible-xs">
-		   				<div class="thumbnail">
-	   						<img src="images/rubrix-1.jpg" width="400" height="587">
-		   				</div>
-		   				<div class="thumbnail">
-		   					<img src="images/rubrix-2.jpg" width="400" height="587">
-		   				</div>
-		   				<div class="thumbnail">
-		   					<img src="images/rubrix-3.jpg" width="400" height="587">
-		   				</div>
-	   				</div>
-	   				<p>Rubrix is a robust and flexible platform that lets you evaluate virtually anything. By creating your own custom Rubrics for a specific subject you can use them to evaluate your boss, your students, or even the beer you are drinking.</p>
-	   				<p>When I first began work at Discovery Software I was fairly inexperienced and had a lot of new tools to learn. However, since I have a strong background in Computer Information Systems, and a desire to learn, I quickly learned the tools and improved all of my skills. After a few months, I had learned to work with Git, LESS, and improved my abilities with JavaScript and CSS. Soon I was designing new user interfaces that interacted with a RESTful API, and pitching new ideas to help improve the application.</p>
-	   				<p>My most recent work on Rubrix involved updating the chrome and incorporating bootstrap across the entire application. This involved updating 100+ files in which code was cleaned up, and the interface was redesigned where necessary.  The screenshots here display the new Rubrix chrome that will be released in the near future</p>
-	   				<p>
-	   			</div>
-				<div class="col-sm-6 col-md-3">
-					<div data-value="20" class="chart breakdown text-center col-sm-6 col-md-12">
-	   					<canvas width="100" height="100"></canvas>
-	   					<div>
-	   						<i class="fa fa-flask"></i>
-	   						<p>DESIGN</p>
-	   					</div>
-	   				</div>
-	   				<div data-value="35" class="chart breakdown text-center col-sm-6 col-md-12">
-	   					<canvas width="100" height="100"></canvas>
-	   					<div>
-	   						<i class="fa fa-css3"></i>
-	   						<p>CSS3</p>
-	   					</div>
-	   				</div>
-	   				<div data-value="30" class="chart breakdown text-center col-sm-6 col-md-12">
-	   					<canvas width="100" height="100"></canvas>
-	   					<div>
-	   						<i class="fa fa-rocket"></i>
-	   						<p>JS</p>
-	   					</div>
-	   				</div>
-	   				<div data-value="20" class="chart breakdown text-center col-sm-6 col-md-12">
-	   					<canvas width="100" height="100"></canvas>
-	   					<div>
-	   						<i class="fa fa-cogs"></i>
-	   						<p class="php">PHP/SQL</p>
-	   					</div>
-	   				</div>
-				</div>		   		
-	   		</article>
-	   		<article class="row slide-right poster">
-	   			<div class="hidden-xs col-sm-6 col-md-3">
-	   				<div class="thumbnail">
-	   					<img src="images/poster-full.jpg" width="400" height="587">
-	   				</div>
-	   			</div>
-	   			<div class="col-xs-12 col-sm-6 col-md-6">
-	   				<h3>Poster Design Contest
-	   					<a class="slide-back pull-right hint" title="Back">
-		   					<i class="fa fa-chevron-circle-left"></i>
-	   					</a>
-	   				</h3>
-	   				<div class="col-xs-6 pull-left visible-xs">
-		   				<div class="thumbnail">
-		   					<img src="images/poster-full.jpg" width="400" height="587">
-		   				</div>
-	   				</div>
-	   				<p>This was a poster design contest held at Discovery Software. The idea was to encourage programmers and designers to think more about design and how to present information.<p>
-	   				<p>The only constraint was that each poster had to have something to do with what we do at Discovery Software. Since we produce a lot of code, I choose the route of a motivational poster to help inspire excellent code.</p>
-	   				<p>An additional challenge was proposed to the web development team to produce the poster using only HTML and CSS. This poster does just that using only HTML and CSS combined with Font Awesome.</p>
-	   				<p>
-	   			</div>
-				<div class="col-sm-6 col-md-3">
-					<div data-value="50" class="chart breakdown text-center col-sm-4 col-md-12">
-	   					<canvas width="100" height="100"></canvas>
-	   					<div>
-	   						<i class="fa fa-flask"></i>
-	   						<p>DESIGN</p>
-	   					</div>
-	   				</div>
-	   				<div data-value="35" class="chart breakdown text-center col-sm-4 col-md-12">
-	   					<canvas width="100" height="100"></canvas>
-	   					<div>
-	   						<i class="fa fa-css3"></i>
-	   						<p>CSS3</p>
-	   					</div>
-	   				</div>
-	   				<div data-value="15" class="chart breakdown text-center col-sm-4 col-md-12">
-	   					<canvas width="100" height="100"></canvas>
-	   					<div>
-	   						<i class="fa fa-html5"></i>
-	   						<p>HTML5</p>
-	   					</div>
-	   				</div>
-				</div>		   		
-	   		</article>
-	   		<article class="row slide-right hughes">
-		   		<p><a class="slide-back btn btn-default">Back</a></p>
-	   		</article>
-	   		<article class="row slide-right woody">
-	   			<div class="col-md-12">
-	   				<p><a class="slide-back btn btn-default">Back</a></p>
-	   			</div>
-	   		</article>
-	   		
-	   		<article class="slider-nav row">
-		   		<div class="col-sm-6 col-md-3">
-		   			<div class="thumbnail">
-		   				<img src="images/rubrix.jpg" width="300" height="200">
-		   				<div class="caption">
-		   					<h3>Rubrix</h3>
-		   					<p>Rubrix is a customizable evaluation tool by Discovery Software. My work focussed on UI design and development. </p>
-		   				</div>
-		   				<div class="caption more">
-		   					<a data-target="rubrix" class="slide btn btn-default">More info <i class="fa fa-chevron-circle-right"></i></a>
-		   				</div>
-		   				
-		   				
-		   			</div>
-		   		</div>
-		   		<div class="col-sm-6 col-md-3">
-		   			<div class="thumbnail">
-		   				<img src="images/poster.jpg" width="300" height="200">
-		   				<div class="caption">
-		   					<h3>Design Contest</h3>
-		   					<p>A finalist submission for a workplace poster design contest. This was produced purely with HTML and CSS.   </p>
-		   					
-		   				</div>
-		   				<div class="caption more">
-		   					<a data-target="poster" class="slide btn btn-default">More info <i class="fa fa-chevron-circle-right"></i></a>
-		   				</div>
-		   				
-		   			</div>
-		   		</div>
-		   		<div class="clearfix visible-sm"></div>
-		   		<div class="col-sm-6 col-md-3">
-		   			<div class="thumbnail">
-		   				<img src="images/katehughes.jpg" width="300" height="200">
-		   				<div class="caption">
-		   					<h3>Kate Hughes Design</h3>
-		   					<p>A single page website full of precisely timed animations. Animation sequencing is handled using jQuery.</p>
-		   				</div>
-		   				<div class="caption more">
-		   					<a data-target="hughes" class="slide btn btn-default">More info <i class="fa fa-chevron-circle-right"></i></a>
-		   				</div>
-		   			</div>
-		   		</div>
-		   		<div class="col-sm-6 col-md-3">
-		   			<div class="thumbnail">
-		   				<img src="images/woodysed.jpg" width="300" height="200">
-		   				<div class="caption">
-		   					<h3>Woody Sed</h3>
-		   					<p>A gallery for a travelling production. Uses Supersized for full screen background slideshows, with a custom gallery page.</p>
-		   				</div>
-		   				<div class="caption more">
-		   					<a data-target="woody" class="slide btn btn-default">More info <i class="fa fa-chevron-circle-right"></i></a>
-		   				</div>
-		   				
-		   			</div>
-		   		</div>
-	   		</article>
-	   </div>
-   </section>
-   <section id="work-experience">
-	   	<div class="container">
-		   <h2>Work Experience</h2>
-		   <article class="row">
-			   <div class="logo col-md-6 col-sm-3 hidden-xs">
-			   		<div class="rubrix-logo">
-			   			<div class="square">
-				   			<p class="r">r</p>
-			   			</div>
-			   			<div class="circle"></div>
-			   			<div class="rubrix-i"></div>
-			   			<p class="rubrix">rubrix</p>
-			   		</div>
-			   </div>
-			   <div class="col-md-6 col-sm-9">
-			   		<h3>
-			   			Web developer - Discovery Software
-			   		</h3>
-			   		<p class="text-muted">
-			   			2012 - Present
-			   		</p>
-			   		<p>As part of the Rubrix team I primarily develop user interfaces that interact with our RESTful API. My latest work has involved updating the interface across the entire application to use a new chrome, and use Twitter Bootstrap</p>
-			   		<a class="btn btn-default">
-			   			More info
-			   		</a>
-			   </div>
-		   </article>
-		   <article class="row">
-			   <div class="col-md-6 col-sm-9">
-				   <h3>
-				   		Web developer - eVision Media
-				   </h3>
-				   <p class="text-muted">
-				   		2010 - 2012
-				   </p>
-				   <p>Developed websites to meet specific client needs. My focus here ranged from developing back end functionality with PHP and MySQL to implementing the user interface based on a designers image. A variety of frameworks were used here including WordPress, Drupal, Magento, and CakePHP</p>
-				   <a class="btn btn-default">
-				   	More info
-				   </a>
-			   </div>
-			   <div class="col-md-6 col-sm-3 hidden-xs">
-			   		<div class="techno-logos">
-			   			<object type="image/svg+xml" data="svg/wordpress.svg" class="drupal logo"></object>
-			   			<object type="image/svg+xml" data="svg/magento.svg" class="magento logo"></object>
-			   			<object type="image/svg+xml" data="svg/drupal.svg" class="drupal logo"></object>
-			   			<object type="image/svg+xml" data="svg/cakephp.svg" class="cakephp logo"></object>
-			   		</div>
-			   </div>
-		</article>
-   </section>
-   <section id="about">
-   	<div class="container">
-   		<h2>About Me</h2>
-   		<article class="row">
-	   		<div class="col-md-12">
-	   			<p>I have been working as a professional web developer with 3+ years of experience. I obtained my Bachelor's of Computer Information Systems in 2010. My studies did not strictly focus on web development, they focused on principles of software development and programming. This has given me a solid foundation to work with in the world of web development which forces you to dive into more than five different programming languages on a daily basis, and keep up with new technology and frameworks.</p>
-	   		</div>
-   		</article>
-   	</div>
-   </section>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
     <script src="js/Chart.min.js"></script>
@@ -358,10 +79,15 @@
 		    		}, 1500);
 	    	});
     	}
-
+    	function scrollTo(element) {
+	    	$('html, body').animate({
+		    	scrollTop: element.offset().top - 50
+	    	}, 500);
+    	}
     	$(document).ready(function(e) {
 	    	$('.btn.slide').click(function() {
-		    	var btn = $(this);
+		    	var btn = $(this),
+		    		slider = btn.closest('.slider');
 		    	btn.closest('.row')
 		    		.addClass('slide-left')
 		    		.parent()
@@ -369,7 +95,10 @@
 		    		.find('.' + btn.attr('data-target'))
 		    		.removeClass('slide-right');
 		    	runCharts(btn.closest('.container').find('.' + btn.attr('data-target') + ' .chart'));
-
+		    	
+		    	setTimeout(function() {
+		    		scrollTo(slider);
+		    	}, 500);
 	    	});
 	    	$('a.slide-back').click(function() {
 		    	var btn = $(this);
